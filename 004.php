a4c8a551258efae0cd5738a8b4412e04c1406398
Wahyu Wibowo <wahyuoxs135@gmail.com>
2020-07-06 16:27:39 +0700

    revisi pertama

--- 004.php
+++ 004.php
@@ -1,21 +1,22 @@
 <?php
 
-function myfunction($v)
-{
-    return($v*$v);
-}
-
 $products = [
-    "grape"=>"fruit",
-    "apple"=>"fruit",
-    "spinach"=>"vegetable",
-    "eggplant"=>"vegetable"
+    "grape" => "fruit",
+    "apple" => "fruit",
+    "spinach" => "vegetable",
+    "eggplant" => "vegetable"
 ];
-
-array_fill();
-// array_keys($products);
-// array_filter($products);
-
-var_dump($products);
-// $a=array(1,2,3,4,5);
-// print_r(array_map("myfunction",$a));
+$category = array();
+foreach ($products as $cat) {
+    array_push($category, $cat);
+}
+$category = array_unique($category);
+$data = array();
+foreach ($products as $key => $value) {
+    foreach ($category as $kategori) {
+        if ($value == $kategori) {
+            $data[$kategori][] = $key;
+        }
+    }
+}
+print_r($data);
